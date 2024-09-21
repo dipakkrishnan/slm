d9674cb0f70ae4d3aa4a8b850c24a29c313df24e
Dipak Krishnan <dipakkrishnan@Dipaks-MBP-2.lan>
2024-09-20 23:18:09 -0400

    implement preprocessor and tokenizer config

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,20 +1,41 @@
-from datasets import load_dataset
 import os
+from datasets import load_dataset, Dataset
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
 
-# Load the Wikitext-2 dataset
-dataset = load_dataset('wikitext', 'wikitext-2-raw-v1')
 
-# Save train, validation, and test splits to separate files
-def save_data(split: list[str], file_name: str):
-    os.makedirs('data', exist_ok=True)
-    file_path = os.path.join('data', file_name)
-    with open(file_path, "w") as f:
-        for line in split:
-            f.write(line + "\n")
+def train_tokenizer(
+    train_split_file: str = "dataset_splits/train.txt",
+    tokenizer_config_file: str = "tokenizer/tokenizer.json",
+):
+    """Trains tokenizer using byte-pair encoding algorithm."""
+    tokenizer = Tokenizer(BPE())
+    tokenizer.pre_tokenizer = Whitespace()  # split into words on whitespace
+    trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+    tokenizer.train(files=[train_split_file], trainer=trainer)
+    tokenizer.save(tokenizer_config_file)  # persist to disk
 
-# Save the train, validation, and test datasets
-save_data(dataset['train']['text'], os.path.join("train_data.txt"))
-save_data(dataset['validation']['text'], "validation_data.txt")
-save_data(dataset['test']['text'], "test_data.txt")
 
-print("Data saved successfully!")
+def save_to_disk(dataset: Dataset, output_dir: str = "dataset_splits"):
+    """Saves dataset splits to disk."""
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for split in dataset:
+        file_path = os.path.join(output_dir, f"{split}.txt")
+        with open(file_path, "w", encoding="utf-8") as f:
+            for item in dataset[split]["text"]:  # save each split to disk
+                f.write(item + "\n")
+
+
+def main():
+    """Main handler to load dataset, save it to disk and train tokenizer."""
+    dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
+    save_to_disk(dataset)
+    train_tokenizer()
+
+
+if __name__ == "__main__":
+    main()
